d25280f9ecb9be805d42f7a57be8baa2081a7dd3
Dennis Bækgaard <dbni11@student.aau.dk>
2014-10-16 11:49:33 +0200

    Basics works

---
+++ new Game Folder/Assets/Scripts/Despawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Despawner : MonoBehaviour {
+
+    public GameObject groundPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("BOOM!");
+
+        
+        Instantiate(groundPrefab, new Vector3(0,0,other.gameObject.transform.position.z + 60), new Quaternion());
+        Debug.Log("After instantiate");
+        Destroy(other.gameObject);
+    }
+}
---
+++ new Game Folder/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    private float rotationSpeed = 50f;
+    private float movementSpeed = 5f;
+
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+	}
+}

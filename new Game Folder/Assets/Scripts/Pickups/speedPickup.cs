5dc639be4f00daf6219ffd0fb3909d6ccea8c446
Dennis Bækgaard <dbni11@student.aau.dk>
2014-10-16 15:11:57 +0200

    Pickup

---
+++ new Game Folder/Assets/Scripts/Pickups/speedPickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speedPickup : PickupBase {
+
+    public GameObject player;
+
+    public override void OnPickedUp()
+    {
+
+        player.GetComponent<PlayerControl>().movementSpeed *= 5;
+
+    }
+}

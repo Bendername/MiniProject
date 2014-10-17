a7ae5a37bd62388253281918228ffc8f6085ce7a
Dennis Bækgaard <dbni11@student.aau.dk>
2014-10-17 13:07:48 +0200

    Crazy Town

---
+++ new Game Folder/Assets/Scripts/CrazyTown.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrazyTown : MonoBehaviour {
+
+    public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+
+    public void ChangeMaterial()
+    {
+        foreach(Transform child in transform )
+            child.gameObject.renderer.material = materials[Random.Range(0, 6)];
+    }
+}

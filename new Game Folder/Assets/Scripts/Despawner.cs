26b94892ae915bd27eb35ac27dfc6d13e7308612
Dennis Bækgaard <dbni11@student.aau.dk>
2014-10-20 09:42:39 +0200

    Added controlled spawn positioning for boidPickups

--- new Game Folder/Assets/Scripts/Despawner.cs
+++ new Game Folder/Assets/Scripts/Despawner.cs
@@ -10,10 +10,16 @@ public class Despawner : MonoBehaviour {
     [SerializeField]
     GameObject boidPickup;
 
-
+    PickupSpawner pSpawner;
     public GameObject endScreen;
     public GameObject speedPowerUp;
     public GameObject crazyRotationPowerUp;
+
+    void Awake()
+    {
+        pSpawner = new PickupSpawner(new Vector2(8f, 8f), new Vector2(-4, 1));
+    }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -37,7 +43,8 @@ public class Despawner : MonoBehaviour {
                 Debug.Log("New position: " + other.transform.position);
                 int speedPickupRng = Random.Range(0, 100);
                 int crazyRotationPickupRng = Random.Range(0, 100);
-                
+
+
                 Instantiate(TestEnemy, new Vector3(Random.Range(-4, 5), Random.Range(1, 10), other.gameObject.transform.position.z + 100), new Quaternion());
                 if (speedPickupRng < GameMaster.GetPowerUpValues().spawnSpeedChancePercent)
                 {
@@ -48,8 +55,9 @@ public class Despawner : MonoBehaviour {
                 }
 
                 
+                Vector2 boidPos = pSpawner.GetNextBoidPosition();
 
-                Instantiate(boidPickup, new Vector3(Random.Range(-4, 5), Random.Range(1, 10), other.gameObject.transform.position.z + 100), new Quaternion());
+                Instantiate(boidPickup, new Vector3(boidPos.x, boidPos.y, other.gameObject.transform.position.z + 100), new Quaternion());
             }
         }
         else
---
+++ new Game Folder/Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSpawner {
+
+    
+    Vector2 lastPosition;
+    Vector2 maxCoords;
+    Vector2 offset;
+    
+	public PickupSpawner(Vector2 size, Vector2 offset)
+    {
+        maxCoords = size;
+        this.offset = offset;
+    }
+
+    public Vector2 GetNextBoidPosition()
+    {
+        if (lastPosition == null)
+        {
+            lastPosition = new Vector2(Random.Range(0f, maxCoords.x), Random.Range(0f, maxCoords.y));
+        }
+        else
+        {
+            lastPosition.x += Random.Range(-1f, 1f);
+            lastPosition.y += Random.Range(-1f, 1f);
+
+            if (lastPosition.x > maxCoords.x)
+                lastPosition.x = maxCoords.x;
+            else if (lastPosition.x < 0)
+                lastPosition.x = 0;
+
+
+            if (lastPosition.y > maxCoords.y)
+                lastPosition.y = maxCoords.y;
+            else if (lastPosition.y < 0)
+                lastPosition.y = 0;
+        }
+
+        return lastPosition + offset;
+    }
+
+    
+}

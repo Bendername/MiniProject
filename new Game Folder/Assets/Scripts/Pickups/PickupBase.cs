47164cbcc9b341bcd7f5350411492c9d679d0d8c
bhuberDADIU <bhuber11@student.aau.dk>
2014-10-20 11:42:43 +0200

    Better transparency

--- new Game Folder/Assets/Scripts/Pickups/PickupBase.cs
+++ new Game Folder/Assets/Scripts/Pickups/PickupBase.cs
@@ -37,7 +37,7 @@ public class PickupBase : MonoBehaviour
 
 		if (fadeOut) {
 			Color clr = mat.color;
-			clr.a = Vector3.Distance(GameMaster.playerObject.transform.position,transform.position) / FadeSpeed;
+			clr.a = Vector3.Distance(GameMaster.playerObject.transform.position,transform.position) / FadeSpeed + 0.5f;
 			mat.color = clr;
 		}
 

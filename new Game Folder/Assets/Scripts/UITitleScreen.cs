2597ee44488e848f827714fe7c865b90f974e0b6
Dennis Bækgaard <dbni11@student.aau.dk>
2014-10-21 09:32:32 +0200

    saved highscore retreived

---
+++ new Game Folder/Assets/Scripts/UITitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITitleScreen : MonoBehaviour {
+
+    public Text highscore;
+
+	// Use this for initialization
+	void Start () {
+        highscore.text = PlayerPrefs.GetFloat("highscore").ToString(); ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

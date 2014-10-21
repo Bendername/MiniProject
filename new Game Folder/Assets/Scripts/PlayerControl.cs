84b52debb1bbd37eca31c68742dbedff4a5691ee
bhuberDADIU <bhuber11@student.aau.dk>
2014-10-21 13:00:30 +0200

    sound fix

--- new Game Folder/Assets/Scripts/PlayerControl.cs
+++ new Game Folder/Assets/Scripts/PlayerControl.cs
@@ -62,7 +62,7 @@ public class PlayerControl : MonoBehaviour {
     public static void AddMoveSpeedBonus(float bonus)
     {
         staticRef.movementSpeed += bonus;
-		staticRef.bgMusic.pitch = staticRef.movementSpeed / 25;
+		staticRef.bgMusic.pitch = 1 + staticRef.movementSpeed / 50;
     }
 
     public static float GetMovementSpeed()
